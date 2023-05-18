7d881a0b9c72defeedc72a3d1b39e7d01caaa34d
ahmedhimel21 <ahmedhimel000@gmail.com>
2023-05-18 11:29:08 +0600

    register and 404 page implemented

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,8 @@ import Main from "../Layouts/Main";
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
 import LoginLayout from "../Layouts/LoginLayout";
+import Register from "../Pages/Register/Register";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +22,18 @@ const router = createBrowserRouter([
       path: '/login',
       element: <Login></Login>
     }]
+  },
+  {
+    path: '/register',
+    element: <LoginLayout></LoginLayout>,
+    children: [{
+      path: '/register',
+      element: <Register></Register>
+    }]
+  },
+  {
+    path: '*',
+    element: <ErrorPage></ErrorPage>
   }
 ]);
 
