c1e5c7dfaf7d1b5e767aa02b6aa102c7d76571e0
ahmedhimel21 <ahmedhimel000@gmail.com>
2023-05-18 19:10:37 +0600

    navigationbar login & photoUrl conditional rendering implemented

---
+++ src/Layouts/MyToysLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavigationBar from '../Shared/Header/NavigationBar';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Shared/Footer/Footer';
+
+const MyToysLayout = () => {
+  return (
+    <div>
+      <NavigationBar></NavigationBar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default MyToysLayout;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -5,6 +5,14 @@ import Login from "../Pages/Login/Login";
 import LoginLayout from "../Layouts/LoginLayout";
 import Register from "../Pages/Register/Register";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import AllToysLayout from "../Layouts/AllToysLayout";
+import AllToys from "../Pages/AllToys/AllToys";
+import MyToysLayout from "../Layouts/MyToysLayout";
+import MyToys from "../Pages/MyToys/MyToys";
+import AddAToyLayout from "../Layouts/AddAToyLayout";
+import AddAToy from "../Pages/AddAToy/AddAToy";
+import BlogsLayout from "../Layouts/BlogsLayout";
+import Blogs from "../Pages/Blogs/Blogs";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +21,38 @@ const router = createBrowserRouter([
     children: [{
       path: '/',
       element: <Home></Home>
+    }]
+  },
+  {
+    path: '/allToys',
+    element: <AllToysLayout></AllToysLayout>,
+    children: [{
+      path: '/allToys',
+      element: <AllToys></AllToys>
+    }]
+  },
+  {
+    path: '/myToys',
+    element: <MyToysLayout></MyToysLayout>,
+    children: [{
+      path: '/myToys',
+      element: <MyToys></MyToys>
+    }]
+  },
+  {
+    path: '/addAToy',
+    element: <AddAToyLayout></AddAToyLayout>,
+    children: [{
+      path: '/addAToy',
+      element: <AddAToy></AddAToy>
+    }]
+  },
+  {
+    path: '/blogs',
+    element: <BlogsLayout></BlogsLayout>,
+    children: [{
+      path: '/blogs',
+      element: <Blogs></Blogs>
     }]
   },
   {
