411d5eea14994e12aa26880da624ce8761af7faf
ahmedhimel21 <ahmedhimel000@gmail.com>
2023-05-18 18:19:19 +0600

    firebase login and register method implemented

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,21 +1,89 @@
-import React from "react";
-import Lottie from 'lottie-react';
-import login from '../../assets/login.json'
+import Lottie from "lottie-react";
+import login from "../../assets/login.json";
+import useTitle from "../../hooks/useTitle";
+import { FaGoogle } from "react-icons/fa";
+import React, { useContext, useRef, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { sendPasswordResetEmail } from "firebase/auth";
+import { AuthContext } from "../../Provider/AuthProviders";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const Login = () => {
+  const { signInWithGoogle, signIn, auth } = useContext(AuthContext);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [show, setShow] = useState(false);
+  const emailRef = useRef();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+  const handleSignIn = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    signIn(email, password)
+      .then((r) => {
+        const loggedUser = r.user;
+        console.log(loggedUser);
+        navigate(from, { replace: true });
+        setError("");
+        form.reset();
+        setSuccess("Login Successfully");
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error.message);
+        setSuccess("");
+      });
+  };
+  //social login
+  const handleGoogleSignIn = () => {
+    signInWithGoogle()
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const handleResetPassword = (e) => {
+    const email = emailRef.current.value;
+    if (!email) {
+      toast("please provide your email address to reset password", {
+        autoClose: 500,
+      });
+      return;
+    }
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        alert("Please check your email");
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error.message);
+      });
+  };
+  useTitle("Login");
   return (
     <div className="mx-8 my-8">
-      <div className="flex flex-col md:flex-row h-screen">
+      <div className="flex flex-col md:flex-row">
         {/* Left side with company name and text */}
         <div
           className="bg-blue-500 text-white p-10 flex flex-col justify-center items-center md:w-1/2 rounded-lg"
           // style={{ backgroundImage: 'url("login1.jpg")' }}
         >
-        <Lottie animationData={login} loop={true} className="h-[500px]"></Lottie>
+          <Lottie
+            animationData={login}
+            loop={true}
+            className="h-[500px]"
+          ></Lottie>
         </div>
 
         {/* Right side with login form */}
         <div className="bg-gray-200 p-10 flex justify-center items-center md:w-1/2 rounded-lg">
-          <form className="w-full max-w-sm">
+          <form onSubmit={handleSignIn} className="w-full max-w-sm">
             <h2 className="text-3xl font-bold mb-6">Login</h2>
             <div className="mb-4">
               <label
@@ -29,6 +97,9 @@ const Login = () => {
                 id="email"
                 type="email"
                 placeholder="Enter your email"
+                name="email"
+                ref={emailRef}
+                required
               />
             </div>
             <div className="mb-6">
@@ -43,31 +114,50 @@ const Login = () => {
                 id="password"
                 type="password"
                 placeholder="Enter your password"
+                name="password"
+                required
               />
             </div>
+            <p className="text-red-600">{error}</p>
             <div className="flex items-center justify-between mb-6">
               <button
+                type="submit"
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="button"
               >
                 Sign In
               </button>
               <a
+                onClick={handleResetPassword}
                 className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
                 href="#"
               >
                 Forgot Password?
               </a>
+              <ToastContainer />
             </div>
             <div>
               <p className="text-gray-700 mb-2">Or sign in with:</p>
               <div className="flex gap-2">
                 {/* Social media login buttons/icons */}
-                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                  <i className="fab fa-google mr-2"></i> Google
+                <button
+                  onClick={handleGoogleSignIn}
+                  className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center"
+                >
+                  <FaGoogle className="mr-2"></FaGoogle> Google
                 </button>
               </div>
             </div>
+            <p>
+              <small className="text-lg font-medium">
+                New to Avenger Toys?{" "}
+                <Link
+                  to="/register"
+                  className="text-xl font-semibold text-blue-500"
+                >
+                  Register
+                </Link>
+              </small>
+            </p>
           </form>
         </div>
       </div>
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,14 +1,87 @@
-import React from "react";
-import Lottie from 'lottie-react';
-import rocket from '../../assets/roctet.json'
+import Lottie from "lottie-react";
+import rocket from "../../assets/roctet.json";
+import { FaGoogle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { updateProfile } from "firebase/auth";
+import { AuthContext } from "../../Provider/AuthProviders";
+import useTitle from "../../hooks/useTitle";
 
 const Register = () => {
+  const { createUser, signInWithGoogle, user } = useContext(AuthContext);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [show, setShow] = useState(false);
+  const handleCreateUser = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const photoUrl = form.photo.value;
+    const email = form.email.value;
+    const password = form.userPassword.value;
+    const confirm = form.confirmPassword.value;
+    if (!/(?=.*[A-Z])/.test(password)) {
+      setError("Please add at least one uppercase");
+      return;
+    } else if (!/(?=.*[!@#$&*])/.test(password)) {
+      setError("Please add a special characters");
+      return;
+    } else if (password.length < 6) {
+      setError("Please add at least 8 Characters");
+      return;
+    } else if (password !== confirm) {
+      alert("Please make sure your password are same!");
+      return;
+    }
+    createUser(email, password)
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        form.reset();
+        setSuccess("User has been created successfully");
+        // handleEmailVerification(result.user);
+        updateUserData(result.user, name, photoUrl);
+        setError("");
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error.message);
+        setSuccess("");
+      });
+  };
+  const handleGoogleSignIn = () => {
+    signInWithGoogle()
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        setSuccess("User has been created successfully");
+        setError("");
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error.message);
+        setSuccess("");
+      });
+  };
+  const updateUserData = (user, name, photoURL) => {
+    updateProfile(user, {
+      displayName: name,
+      photoURL: photoURL,
+    })
+      .then(() => {
+        setSuccess("user profile updated");
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+  useTitle("Register");
   return (
-    <div>
-      <div className="flex flex-col md:flex-row h-screen">
+    <div className="mx-8 my-3">
+      <div className="flex flex-col md:flex-row rounded-lg">
         {/* Left side with register form */}
         <div className="bg-gray-200 p-10 flex justify-center items-center md:w-1/2 rounded-lg">
-          <form className="w-full max-w-sm">
+          <form onSubmit={handleCreateUser} className="w-full max-w-sm">
             <h2 className="text-3xl font-bold mb-6">Register</h2>
             <div className="mb-4">
               <label
@@ -22,6 +95,8 @@ const Register = () => {
                 id="name"
                 type="text"
                 placeholder="Enter your name"
+                name="name"
+                required
               />
             </div>
             <div className="mb-4">
@@ -36,6 +111,8 @@ const Register = () => {
                 id="email"
                 type="email"
                 placeholder="Enter your email"
+                name="email"
+                required
               />
             </div>
             <div className="mb-6">
@@ -48,26 +125,56 @@ const Register = () => {
               <input
                 className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="password"
-                type="password"
+                type={show ? "text" : "password"}
                 placeholder="Enter your password"
+                name="userPassword"
+                required
               />
+              <p onClick={() => setShow(!show)} className="my-3 text-md font-bold">
+                  {show ? (
+                    <span className="cursor-pointer	">Hide password!</span>
+                  ) : (
+                    <span className="cursor-pointer	">Show password!</span>
+                  )}
+                </p>
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
                 htmlFor="password"
               >
+                Confirm Password
+              </label>
+              <input
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="Confirm password"
+                type={show ? "text" : "password"}
+                placeholder="Confirm password"
+                name="confirmPassword"
+                required
+              />
+              <p onClick={() => setShow(!show)} className="my-3 text-md font-bold">
+                  {show ? (
+                    <span className="cursor-pointer	">Hide password!</span>
+                  ) : (
+                    <span className="cursor-pointer	">Show password!</span>
+                  )}
+                </p>
+              <label className="block text-gray-700 text-sm font-bold mb-2">
                 Photo
               </label>
               <input
                 className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="password"
-                type="password"
+                type="text"
                 placeholder="Photo URL"
+                name="photo"
               />
             </div>
+            <p className="text-red-600 mb-2">{error}</p>
+            <p className="text-green-600 mb-2">{success}</p>
             <div className="flex items-center justify-center">
               <button
+                type="submit"
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="button"
               >
                 Register
               </button>
@@ -76,11 +183,26 @@ const Register = () => {
               <p className="text-gray-700 mb-2">Or register with:</p>
               <div className="flex items-center justify-center">
                 {/* Social media login buttons/icons */}
-                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                  <i className="fab fa-google mr-2"></i> Google
+                <button
+                  onClick={handleGoogleSignIn}
+                  className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center mb-3"
+                >
+                  <FaGoogle className="mr-2"></FaGoogle> Google
                 </button>
               </div>
             </div>
+            <p>
+              <small className="text-lg font-medium text-center">
+                Already Have an account?{" "}
+                <Link
+                  to="/login"
+                  className="text-xl font-semibold text-blue-500"
+                >
+                  Login
+                </Link>
+              </small>
+            </p>
+           
           </form>
         </div>
 
@@ -89,7 +211,11 @@ const Register = () => {
           className="bg-cover bg-center md:w-1/2"
           // style={{ backgroundImage: 'url("register.webp")' }}
         >
-          <Lottie animationData={rocket} loop={true} className="h-[562px] w-full"></Lottie>
+          <Lottie
+            animationData={rocket}
+            loop={true}
+            className="h-[562px] w-full"
+          ></Lottie>
         </div>
       </div>
     </div>
